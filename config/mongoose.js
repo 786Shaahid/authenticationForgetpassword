4cc7f6038a77c06ae6884984f57bbdf2440fe2e5
Shahid Raza <shahidraza02qwert@gmail.com>
2023-06-05 22:51:37 +0530

    name of database

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@
   //   console.log("Connected successfully to database!");
   // });
   const mongoose=require("mongoose");
-  const url= `mongodb+srv://shahidraza02qwert:${process.env.MONGODB_PASSWORD}@cluster0.mqrfg6o.mongodb.net/${process.env.MONGODB_DATABASENAME}?retryWrites=true&w=majority`
+  const url= `mongodb+srv://shahidraza02qwert:${process.env.MONGODB_PASSWORD}@cluster0.mqrfg6o.mongodb.net/myUserDB?retryWrites=true&w=majority`
 
   mongoose.connect(url);
  const db = mongoose.connection;
